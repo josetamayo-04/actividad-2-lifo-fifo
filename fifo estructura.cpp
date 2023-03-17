3f538405440e413b83daeb74be6b71827f5e5fa3
josetamayo-04 <125928901+josetamayo-04@users.noreply.github.com>
2023-03-16 22:58:14 -0600

    Update fifo estructura.cpp

--- fifo estructura.cpp
+++ fifo estructura.cpp
@@ -1,84 +1,80 @@
 #include <iostream>
 #include <string>
-#include <queue>
 
 using namespace std;
 
-class Paciente {
-    public:
-        string nombre;
-        int numero_documento;
-        Paciente(string n, int dn) {
-            nombre = n;
-            numero_documento = dn;
-        }
+struct Atleta {
+    string nombre;
+    int numero;
 };
 
-void agregarPaciente(queue<Paciente*>& cola) {
-    string nombre;
-    int numero_documento;
-    cout << "Ingrese el nombre del paciente: ";
-    cin >> nombre;
-    while (true) {
-        cout << "Ingrese el n˙mero de documento del paciente: ";
-        cin >> numero_documento;
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(10000, '\n');
-            cout << "Entrada inv·lida. Por favor, ingrese un n˙mero." << endl;
-        }
-        else {
-            break;
-        }
-    }
-    Paciente* p = new Paciente(nombre, numero_documento);
-    cola.push(p);
-}
-
-void mostrarPacientes(queue<Paciente*>& cola) {
-    if (cola.empty()) {
-        cout << "No hay pacientes en la cola." << endl;
-    }
-    else {
-        cout << "Pacientes en la cola:" << endl;
-        int i = 1;
-        while (!cola.empty()) {
-            Paciente* p = cola.front();
-            cola.pop();
-            cout << i << ". " << p->nombre << " (" << p->numero_documento << ")" << endl;
-            delete p;
-            i++;
-        }
-    }
-}
+const int MAX_RONDAS = 10;
 
 int main() {
-    queue<Paciente*> pacientes;
+    // Declaraci√≥n de variables
+    Atleta atletas[MAX_RONDAS];
+    float tiempos[MAX_RONDAS][MAX_RONDAS];
+    float promedios[MAX_RONDAS];
+    int num_atletas = 0;
+    int num_rondas = 0;
 
+    // Bucle principal
     while (true) {
-        cout << "1. Agregar un paciente" << endl;
-        cout << "2. Mostrar todos los pacientes" << endl;
-        cout << "3. Salir" << endl;
-        int opcion;
-        cin >> opcion;
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(10000, '\n');
-            cout << "Entrada inv·lida. Por favor, ingrese un n˙mero." << endl;
-        }
-        else if (opcion == 1) {
-            agregarPaciente(pacientes);
-        }
-        else if (opcion == 2) {
-            mostrarPacientes(pacientes);
-        }
-        else if (opcion == 3) {
+        // Solicitar datos del atleta
+        cout << "Ingrese el nombre del atleta (o 'fin' para terminar): ";
+        string nombre;
+        cin >> nombre;
+        if (nombre == "fin") {
             break;
         }
-        else {
-            cout << "OpciÛn inv·lida. Por favor, ingrese 1, 2 o 3." << endl;
+        cout << "Ingrese el n√∫mero asignado al atleta: ";
+        int numero;
+        cin >> numero;
+
+        // Registrar datos del atleta
+        Atleta atleta = { nombre, numero };
+        atletas[num_atletas] = atleta;
+        num_atletas++;
+
+        // Registrar tiempo en arreglo de tiempos
+        float tiempo;
+        cout << "Ingrese el tiempo del atleta en esta ronda: ";
+        cin >> tiempo;
+        tiempos[num_rondas][num_atletas - 1] = tiempo;
+
+        // Actualizar contador de rondas
+        num_rondas++;
+
+        // Calcular promedios
+        if (num_rondas >= 2) {
+            for (int i = 0; i < num_atletas; i++) {
+                float t1 = tiempos[num_rondas - 1][i];
+                float t2 = tiempos[num_rondas - 2][i];
+                float promedio = (t1 + t2) / 2;
+                promedios[i] = promedio;
+            }
         }
-        cout << endl;
+
+        // Ordenar promedios
+        for (int i = 0; i < num_atletas - 1; i++) {
+            for (int j = i + 1; j < num_atletas; j++) {
+                if (promedios[i] > promedios[j]) {
+                    // Intercambiar elementos
+                    float temp = promedios[i];
+                    promedios[i] = promedios[j];
+                    promedios[j] = temp;
+                    Atleta temp2 = atletas[i];
+                    atletas[i] = atletas[j];
+                    atletas[j] = temp2;
+                }
+            }
+        }
+    }
+
+    // Mostrar resultados finales
+    cout << "Resultados finales:" << endl;
+    for (int i = 0; i < num_atletas; i++) {
+        cout << i + 1 << ". " << atletas[i].nombre << " - Promedio: " << promedios[i] << endl;
     }
 
     return 0;
